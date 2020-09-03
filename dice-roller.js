b501e44758f96609a923c90990dc5d412df7d50b
David <david.he07@yahoo.ca>
2020-09-03 16:58:16 -0400

    implemented lines of code

--- dice-roller.js
+++ dice-roller.js
@@ -2,7 +2,15 @@ let args = process.argv;
 // console.log(args[2]);
 
 const diceRoller = function (numberOfRolls) {
-  console.log(numberOfRolls);
+  // console.log(numberOfRolls);
+  // console.log(numberOfRolls);
+  let answer;
+  for (let i = 0; i < numberOfRolls; i++) {
+    answer = Math.floor(Math.random() * Math.floor(6));
+    console.log(answer);
+  }
+
+  return answer;
 };
 
 diceRoller(args[2]);
