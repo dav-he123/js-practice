0e7818beba7b276f72d7a6b85e7de18792502d86
David <david.he07@yahoo.ca>
2020-09-04 17:02:17 -0400

    updated lines

--- printInFrame.js
+++ printInFrame.js
@@ -7,10 +7,13 @@ const printInFrame = function (list) {
   const border = repeat("*", longest);
 
   console.log(border + "*" + "*" + "*" + "*");
+  // console.log(border);
+
   for (const word of lister) {
     console.log(`* ${word}${repeat(" ", longest - word.length + 1)}*`);
   }
   console.log(border + "*" + "*" + "*" + "*");
+  // console.log(border);
 };
 
 const repeat = function (str, times) {
