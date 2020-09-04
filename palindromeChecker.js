0dc193cd9e0ca30bc1b2348926750c5a78b05125
David <david.he07@yahoo.ca>
2020-09-04 13:23:01 -0400

    successfully debugged the code

---
+++ palindromeChecker.js
@@ -0,0 +1,49 @@
+// FUNCTION IMPLEMENTATION (MULTIPLE BUGS)
+const isPalindrome = function (str) {
+  const noSpaces = str.split(" ").join("");
+  const midIndex = Math.floor(noSpaces.length / 2);
+  const lastIndex = str.length - 1;
+  // console.log(lastIndex);
+  // console.log(midIndex);
+  // console.log(noSpaces);
+  // console.log(lastIndex);
+
+  if (str.length === 1) {
+    return true;
+  }
+  // console.log(midIndex);
+
+  for (let i = 0; i < midIndex; i++) {
+    if (str[i] !== str[lastIndex - i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// Assertion Function
+const assertPalindrome = function (word, expected) {
+  console.log(`Testing isPalindrome(\"${word}\"):`);
+  const actual = isPalindrome(word);
+  if (actual === expected) {
+    console.log("\x1b[32m%s\x1b[0m", `\tPASS ✅ function returned ${actual}\n`);
+  } else {
+    console.log(
+      "\x1b[31m%s\x1b[0m",
+      `\tFAIL 🛑 function returned ${actual} (expected ${expected})\n`
+    );
+  }
+};
+
+// TEST CODE
+// These should all pass assertion, but they don't.
+assertPalindrome("p", true);
+assertPalindrome("racecar", true);
+assertPalindrome("my gym", true);
+assertPalindrome("foo", false);
+assertPalindrome("fluff", false);
+assertPalindrome("just some random words", false);
+
+// Bonus / Stretch: Uncomment these tests and figure out why these are also failing
+// assertPalindrome('Kayak', true);
+// assertPalindrome('a santa at NASA', true);
